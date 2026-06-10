91272dc63bf64bddd7c875fdaf759cc84945ac60
gabrielfrdev <153218445+gabrielfrdev@users.noreply.github.com>
2026-06-10 14:30:00 -0300

    test: enforce 70% line coverage threshold

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      lines: 70,
+    },
+  },
+};
